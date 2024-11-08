c53b9405794311390fdec6d4cfba1ac44fa19803
Kuuw <67199397+Kuuw@users.noreply.github.com>
2024-11-08 10:55:24 +0300

    Add Bcrypt

---
+++ BAL/Concrete/BcryptService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.Concrete
+{
+    public class BcryptService
+    {
+        public bool VerifyPassword(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+    }
+}
--- PL/Controllers/UserController.cs
+++ PL/Controllers/UserController.cs
@@ -1,8 +1,10 @@
 ﻿using BAL.Concrete;
 using DAL.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace PL.Controllers
 {
@@ -11,10 +13,13 @@ namespace PL.Controllers
     public class UserController : ControllerBase
     {
         UserRepository userRepository = new UserRepository();
+        BcryptService bcryptService = new BcryptService();
 
-        [HttpPost]
-        public IActionResult Register(String FirstName, String LastName, String Email, String PasswordHash)
+
+        [HttpPost("Register")]
+        public IActionResult Register(String FirstName, String LastName, String Email, String Password)
         {
+            String PasswordHash = bcryptService.HashPassword(Password);
             User newUser = new User();
             newUser.FirstName = FirstName;
             newUser.LastName = LastName;
@@ -33,6 +38,10 @@ namespace PL.Controllers
             } 
             catch(Exception e) 
             {
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine(e.InnerException.ToString());
+                }
                 return StatusCode(500, new { message = "İsteğinizi işlerken bir hata oluştu.", error = e.Message}); 
             }
         }
