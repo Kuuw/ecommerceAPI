d5af4ee9da8736b2c587a6b1b9ad3bab39ff577e
Kuuw <67199397+Kuuw@users.noreply.github.com>
2024-11-15 14:45:04 +0300

    Add/Update Services

---
+++ BAL/Services/AddressService.cs
@@ -0,0 +1,30 @@
+﻿using DAL.Concrete;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.Services
+{
+    public class AddressService
+    {
+        AddressRepository addressRepository = new AddressRepository();
+        public List<Address> GetAddresses(int UserId)
+        {
+            var adresses = addressRepository.Where(x => x.UserId == UserId).ToList();
+
+            return adresses;
+        }
+
+        public Address AddAddress(Address address)
+        {
+            address.CreatedAt = DateTime.Now;
+            address.UpdatedAt = DateTime.Now;
+            
+            addressRepository.Insert(address);
+            return address;
+        }
+    }
+}
