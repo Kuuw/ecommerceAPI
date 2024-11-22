77e85b42d29191c0c5ee54dd81cfa73706713cc8
Kuuw <67199397+Kuuw@users.noreply.github.com>
2024-11-22 16:23:35 +0300

    Add AutoMapper

---
+++ BAL/Concrete/MapperConfig.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Entities.DTO;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BAL.Abstract;
+
+namespace BAL.Concrete
+{
+    public class MapperConfig: IMapperConfig
+    {
+        public static Mapper InitializeAutomapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<UserRegister, User>();
+            });
+            var mapper = new Mapper(config);
+            return mapper;
+        }
+    }
+}
