2ffbc2b3300b5883810319f0b9bd7de59545625a
Kuuw <67199397+Kuuw@users.noreply.github.com>
2024-11-27 22:34:54 +0300

    Add Delete, GetPaged, GetById, Update functions to ProductService

--- BAL/Abstract/IProductService.cs
+++ BAL/Abstract/IProductService.cs
@@ -9,6 +9,10 @@ namespace BAL.Abstract
 {
     public interface IProductService
     {
-        public Product AddProduct(Product product);
+        public Product Add(Product product);
+        public Product? GetById(int id);
+        public List<Product> GetPaged(int page, int pageSize);
+        public void Delete(int id);
+        public void Update(Product product);
     }
 }
--- BAL/Concrete/ProductService.cs
+++ BAL/Concrete/ProductService.cs
@@ -12,7 +12,7 @@ namespace BAL.Concrete
     public class ProductService: IProductService
     {
         ProductRepository productRepository = new ProductRepository();
-        public Product AddProduct(Product product)
+        public Product Add(Product product)
         {
             product.CreatedAt = DateTime.Now;
             product.UpdatedAt = DateTime.Now;
@@ -23,6 +23,36 @@ namespace BAL.Concrete
             productRepository.AddStockEntry(product.ProductId, 0);
 
             return product;
+        }
+
+        public void Delete(int id)
+        {
+            Product product = productRepository.GetById(id);
+            if (product != null)
+            {
+                productRepository.Delete(product);
+            } else
+            {
+                Console.WriteLine($"Product with id:{id} is not found.");
+            }
+        }
+
+        public List<Product> GetPaged(int page, int pageSize)
+        {
+            var items = productRepository.GetPaged(page, pageSize);
+
+            return items;
+        }
+
+        public Product? GetById(int id)
+        {
+            var item = productRepository.GetById(id);
+            return item;
+        }
+
+        public void Update(Product product)
+        {
+            productRepository.Update(product);
         }
     }
 }
