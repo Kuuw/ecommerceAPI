7b3fac413b2126324cb0ba29392d426627586210
Kuuw <67199397+Kuuw@users.noreply.github.com>
2024-11-28 21:46:29 +0300

    Add new DTOs

---
+++ Entities/DTO/AddressDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTO
+{
+    public class AddressDTO
+    {
+        public int CountryId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string AddressLine1 { get; set; } = null!;
+
+        public string? AddressLine2 { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string Telephone { get; set; }
+    }
+}
---
+++ Entities/DTO/AuthenticateResponse.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTO
+{
+    public class AuthenticateResponse
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Token { get; set; }
+
+
+        public AuthenticateResponse(User user, string token)
+        {
+            UserId = user.UserId;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            Token = token;
+        }
+    }
+}
