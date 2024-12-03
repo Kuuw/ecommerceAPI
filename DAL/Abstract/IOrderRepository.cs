8baa28349a90a4f59bab5351e12ebe2dffb189b5
Kuuw <67199397+Kuuw@users.noreply.github.com>
2024-12-03 11:29:16 +0300

    Update OrderRepository

--- DAL/Abstract/IOrderRepository.cs
+++ DAL/Abstract/IOrderRepository.cs
@@ -4,6 +4,9 @@ namespace DAL.Abstract
 {
     public interface IOrderRepository: IGenericRepository<Order>
     {
-
+        public void InsertItem(OrderItem item);
+        public void UpdateItem(OrderItem item);
+        public void DeleteItem(OrderItem item);
+        public List<OrderItem> GetItems(int OrderId);
     }
 }
--- DAL/Concrete/OrderRepository.cs
+++ DAL/Concrete/OrderRepository.cs
@@ -1,9 +1,35 @@
 ﻿using DAL.Abstract;
 using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DAL.Concrete
 {
-    public class OrderRepository:GenericRepository<Order>, IOrderRepository
+    public class OrderRepository : GenericRepository<Order>, IOrderRepository
     {
+        EcommerceDbContext context = new EcommerceDbContext();
+        DbSet<OrderItem> _orderItems;
+
+        public void DeleteItem(OrderItem item)
+        {
+            _orderItems.Remove(item);
+            context.SaveChanges();
+        }
+
+        public List<OrderItem> GetItems(int OrderId)
+        {
+            return _orderItems.Where(x => x.OrderId == OrderId).ToList();
+        }
+
+        public void InsertItem(OrderItem item)
+        {
+            _orderItems.Add(item);
+            context.SaveChanges();
+        }
+
+        public void UpdateItem(OrderItem item)
+        {
+            _orderItems.Update(item);
+            context.SaveChanges();
+        }
     }
 }
